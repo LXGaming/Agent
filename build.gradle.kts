bb018c975c317520765d76cfdca2ff4490a3db5a
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-02-24 11:28:13 +1300

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("java")
     id("maven-publish")
     id("signing")
-    id("com.gradleup.shadow") version "8.3.4" apply false
+    id("com.gradleup.shadow") version "8.3.6" apply false
 }
 
 subprojects {
